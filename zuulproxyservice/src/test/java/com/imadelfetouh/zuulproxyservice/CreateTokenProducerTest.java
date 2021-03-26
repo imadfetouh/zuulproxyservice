b3a24425a7af7ff8f9b08595d61458277b62a31b
imad <imad.elfetouh@hotmail.com>
2021-03-26 12:56:02 +0100

    test

--- zuulproxyservice/src/test/java/com/imadelfetouh/zuulproxyservice/CreateTokenProducerTest.java
+++ zuulproxyservice/src/test/java/com/imadelfetouh/zuulproxyservice/CreateTokenProducerTest.java
@@ -1,9 +1,14 @@
 package com.imadelfetouh.zuulproxyservice;
 
 import com.imadelfetouh.zuulproxyservice.producer.CreateTokenProducer;
+import com.imadelfetouh.zuulproxyservice.rabbit.RabbitConfiguration;
 import com.imadelfetouh.zuulproxyservice.rabbit.RabbitProducer;
+import com.rabbitmq.client.Channel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class CreateTokenProducerTest {
 
@@ -15,5 +20,17 @@ public class CreateTokenProducerTest {
         String token = rabbitProducer.produce(createTokenProducer);
 
         Assertions.assertNotNull(token);
+    }
+
+    @Test
+    public void testCreateTokenCorrectWithoutRabbitProducer() throws IOException, TimeoutException {
+        Channel channel = RabbitConfiguration.getInstance().getChannel();
+        CreateTokenProducer createTokenProducer = new CreateTokenProducer(1);
+
+        String token = createTokenProducer.produce(channel);
+        channel.close();
+
+        Assertions.assertNotNull(token);
+        Assertions.assertFalse(channel.isOpen());
     }
 }
--- zuulproxyservice/src/test/java/com/imadelfetouh/zuulproxyservice/ZuulproxyserviceApplicationTests.java
+++ zuulproxyservice/src/test/java/com/imadelfetouh/zuulproxyservice/ZuulproxyserviceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.imadelfetouh.zuulproxyservice;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.filter.CorsFilter;
